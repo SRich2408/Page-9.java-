94374c4ecedfda05442542f064132368672756cb
Sebastian Richardson <sebastian.nicolasrg24@outlook.com>
2024-02-07 14:55:36 -0500

    //

---
+++ Page9.java
@@ -0,0 +1,13 @@
+/************************************
+*Project: Sebastian Richardson
+*Date: February 7th
+*Program Name: Page 9.java
+*************************************/
+
+import java.io.*;
+public class page9{
+    public static void main (String[] args){
+        System.out.println("Hi"); //message to user
+        System.out.println("LabOneA"); //assignment
+    }
+}
